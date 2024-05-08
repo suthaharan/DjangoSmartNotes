c35a875804e7d60378c8fcc6f42b3c549d7e2bb9
sutha <sutha@kurinchilion.com>
2024-05-08 17:36:57 -0400

    base form with no styles

--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.NotesListView.as_view(), name="notes.list"),
+    path('new', views.NotesCreateView.as_view(), name="notes.new"),
     #path('<int:pk>', views.detail),
     path('<int:pk>', views.NotesDetailView.as_view(), name="notes.detail")
 ]
